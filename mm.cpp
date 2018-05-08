743b0601fd3fe505b6375bb7424f40d496e68560
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2018-05-08 19:37:07 +1000

    3 matrices seems to be multiplying

--- mm.cpp
+++ mm.cpp
@@ -9,6 +9,8 @@
 #define NUM_COLUMNS_B 12 //columns of input [B]
 #define NUM_ROWS_C 12 //rows of input [C]
 #define NUM_COLUMNS_C 12 //columns of input [C]
+#define NUM_ROWS_P 12 //P is the resultant matrix of A*B
+#define NUM_COLUMNS_P 12 //P is the resultant matrix of A*B
 #define MASTER_TO_SLAVE_TAG 1 //tag for messages sent from master to slaves
 #define SLAVE_TO_MASTER_TAG 4 //tag for messages sent from slaves to master
 #define MASTER_RANK 0 //rank of the master node
@@ -28,7 +30,9 @@ int i, j, k; //helper variables
 double mat_a[NUM_ROWS_A][NUM_COLUMNS_A]; //declare input [A]
 double mat_b[NUM_ROWS_B][NUM_COLUMNS_B]; //declare input [B]
 double mat_c[NUM_ROWS_C][NUM_COLUMNS_C]; //declare input [B]
+double mat_res_1[NUM_ROWS_P][NUM_COLUMNS_P]; //resultant matrix of A*B
 double mat_result[NUM_ROWS_A][NUM_COLUMNS_C]; //declare output [Z]
+
 double start_time; //hold start time
 double end_time; // hold end time
 int low_bound; //low bound of the number of rows of [A] allocated to a slave
@@ -74,7 +78,7 @@ int main(int argc, char * argv[]) {
     for (i = low_bound; i < upper_bound; i++) { //iterate through a given set of rows of [A]
       for (j = 0; j < NUM_COLUMNS_B; j++) { //iterate through columns of [B]
         for (k = 0; k < NUM_ROWS_B; k++) { //iterate through rows of [B]
-          mat_result[i][j] += (mat_a[i][k] * mat_b[k][j]);
+          mat_res_1[i][j] += (mat_a[i][k] * mat_b[k][j]);
         }
       }
     }
@@ -83,8 +87,9 @@ int main(int argc, char * argv[]) {
     //send the upper bound next without blocking, to the master
     MPI_Isend( & upper_bound, 1, MPI_INT, 0, SLAVE_TO_MASTER_TAG + 1, MPI_COMM_WORLD, & request);
     //finally send the processed portion of data without blocking, to the master
-    MPI_Isend( & mat_result[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_B, MPI_DOUBLE, 0, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & request);
+    MPI_Isend( & mat_res_1[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_B, MPI_DOUBLE, 0, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & request);
   }
+  //======
   /* master gathers processed work*/
   if (rank == MASTER_RANK) {
     for (i = 1; i < size; i++) { // untill all slaves have handed back the processed data
@@ -93,8 +98,65 @@ int main(int argc, char * argv[]) {
       //receive upper bound from a slave
       MPI_Recv( & upper_bound, 1, MPI_INT, i, SLAVE_TO_MASTER_TAG + 1, MPI_COMM_WORLD, & status);
       //receive processed data from a slave
-      MPI_Recv( & mat_result[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_B, MPI_DOUBLE, i, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & status);
+      MPI_Recv( & mat_res_1[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_B, MPI_DOUBLE, i, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & status);
     }
+  }
+
+    //prepare for second multiplication
+    if (rank == MASTER_RANK) {
+      for (i = 1; i < size; i++) { //for each slave other than the master
+        portion = (NUM_ROWS_P / (size - 1)); // calculate portion without master
+        low_bound = (i - 1) * portion;
+        if (((i + 1) == size) && ((NUM_ROWS_P % (size - 1)) != 0)) { //if rows of [P] cannot be equally divided among slaves
+          upper_bound = NUM_ROWS_P; //last slave gets all the remaining rows
+        } else {
+          upper_bound = low_bound + portion; //rows of [P] are equally divisable among slaves
+        }
+        //send the low bound first without blocking, to the intended slave
+        MPI_Isend( & low_bound, 1, MPI_INT, i, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, & request);
+        //next send the upper bound without blocking, to the intended slave
+        MPI_Isend( & upper_bound, 1, MPI_INT, i, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, & request);
+        //finally send the allocated row portion of [A] without blocking, to the intended slave
+        MPI_Isend( & mat_res_1[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_P, MPI_DOUBLE, i, MASTER_TO_SLAVE_TAG + 2, MPI_COMM_WORLD, & request);
+      }
+    }
+
+    //broadcast [C] to all the slaves
+    MPI_Bcast( & mat_c, NUM_ROWS_C * NUM_COLUMNS_C, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    /* work done by slaves*/
+    if (rank > MASTER_RANK) {
+      //receive low bound from the master
+      MPI_Recv( & low_bound, 1, MPI_INT, 0, MASTER_TO_SLAVE_TAG, MPI_COMM_WORLD, & status);
+      //next receive upper bound from the master
+      MPI_Recv( & upper_bound, 1, MPI_INT, 0, MASTER_TO_SLAVE_TAG + 1, MPI_COMM_WORLD, & status);
+      //finally receive row portion of [P] to be processed from the master
+      MPI_Recv( & mat_res_1[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_P, MPI_DOUBLE, 0, MASTER_TO_SLAVE_TAG + 2, MPI_COMM_WORLD, & status);
+      for (i = low_bound; i < upper_bound; i++) { //iterate through a given set of rows of [P]
+        for (j = 0; j < NUM_COLUMNS_C; j++) { //iterate through columns of [C]
+          for (k = 0; k < NUM_ROWS_C; k++) { //iterate through rows of [C]
+            mat_result[i][j] += (mat_res_1[i][k] * mat_c[k][j]);
+          }
+        }
+      }
+      //send back the low bound first without blocking, to the master
+      MPI_Isend( & low_bound, 1, MPI_INT, 0, SLAVE_TO_MASTER_TAG, MPI_COMM_WORLD, & request);
+      //send the upper bound next without blocking, to the master
+      MPI_Isend( & upper_bound, 1, MPI_INT, 0, SLAVE_TO_MASTER_TAG + 1, MPI_COMM_WORLD, & request);
+      //finally send the processed portion of data without blocking, to the master
+      MPI_Isend( & mat_result[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_C, MPI_DOUBLE, 0, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & request);
+    }
+
+    /* master gathers processed work*/
+    if (rank == MASTER_RANK) {
+      for (i = 1; i < size; i++) { // untill all slaves have handed back the processed data
+        //receive low bound from a slave
+        MPI_Recv( & low_bound, 1, MPI_INT, i, SLAVE_TO_MASTER_TAG, MPI_COMM_WORLD, & status);
+        //receive upper bound from a slave
+        MPI_Recv( & upper_bound, 1, MPI_INT, i, SLAVE_TO_MASTER_TAG + 1, MPI_COMM_WORLD, & status);
+        //receive processed data from a slave
+        MPI_Recv( & mat_result[low_bound][0], (upper_bound - low_bound) * NUM_COLUMNS_C, MPI_DOUBLE, i, SLAVE_TO_MASTER_TAG + 2, MPI_COMM_WORLD, & status);
+      }
+
     end_time = MPI_Wtime();
     printf("\nRunning Time = %f\n\n", end_time - start_time);
     printArray();
@@ -138,6 +200,13 @@ void printArray() {
     printf("\n");
     for (j = 0; j < NUM_COLUMNS_B; j++)
       printf("%8.2f  ", mat_b[i][j]);
+  }
+  printf("\n\n\n");
+  printf("Matrix A*B:\n");
+  for (i = 0; i < NUM_ROWS_P; i++) {
+    printf("\n");
+    for (j = 0; j < NUM_COLUMNS_P; j++)
+      printf("%8.2f  ", mat_res_1[i][j]);
   }
   printf("\n\n\n");
   printf("Matrix C:\n");
